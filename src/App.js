391bac964e82fab4728b9be023a8714799860859
Dutt Mistry <dutta.mistry@gmail.com>
2023-12-07 22:16:12 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -5,8 +5,17 @@ import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
 
 function App() {
   const [langaugeData, setLangaugeData] = useState([]);
+  const [output, setOutput] = useState("");
+
+  useEffect(() => {
+    // run this command when value changes
+    console.log("output: ", output);
+    document.getElementById("output_of_code").innerText = output?.stdout
+      ? output?.stdout
+      : "";
+  }, [output]);
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  useEffect(async () => {
+  useEffect(() => {
     const url = "https://judge0-ce.p.rapidapi.com/languages";
     const options = {
       method: "GET",
@@ -17,10 +26,12 @@ function App() {
     };
 
     try {
-      const response = await fetch(url, options);
-      const result = await response.text();
-      setLangaugeData(result);
-      console.log("result: ", result);
+      fetch(url, options)
+        .then((res) => res.json())
+        .then((res) => {
+          setLangaugeData(res);
+          console.log("res?.text(): ", res);
+        });
     } catch (error) {
       console.error(error);
     }
@@ -38,17 +49,19 @@ function App() {
       // let outputText = document.getElementById("output");
       // outputText.innerHTML = "";
       // outputText.innerHTML += "Creating Submission ...\n";
-      await fetch("https://judge0-ce.p.rapidapi.com/submissions", {
+      const url =
+        "https://judge0-ce.p.rapidapi.com/submissions?base64_encoded=false&fields=*";
+      await fetch(url, {
         method: "POST",
         headers: {
+          "content-type": "application/json",
+          "Content-Type": "application/json",
           "X-RapidAPI-Key":
             "312a0d93f0msh88b061e76dae04bp1b370ajsn8921fe8ecea6",
           "X-RapidAPI-Host": "judge0-ce.p.rapidapi.com",
-          "content-type": "application/json",
-          accept: "application/json",
         },
         body: JSON.stringify({
-          source_code: btoa(formik?.values?.source_code),
+          source_code: formik?.values?.source_code,
           stdin: null,
           // stdin: btoa(formik?.values?.stdin),
           language_id: formik?.values?.language_id,
@@ -56,21 +69,32 @@ function App() {
       })
         .then((res) => res.json())
         .then(async (res) => {
-          console.log("res: ");
-          let url = `https://judge0-ce.p.rapidapi.com/submissions/${res.token}?base64_encoded=true`;
-          await fetch(url, {
-            method: "GET",
-            headers: {
-              "X-RapidAPI-Key":
-                "312a0d93f0msh88b061e76dae04bp1b370ajsn8921fe8ecea6",
-              "X-RapidAPI-Host": "judge0-ce.p.rapidapi.com",
-              "content-type": "application/json",
-            },
-          })
-            .then((res) => res.json())
-            .then((res) => {
-              console.log("res: ", res);
-            });
+          if (res?.token) {
+            console.log("res: ", res);
+            console.log("res.token: ", res.token);
+            // let url =
+            //   "https://judge0-ce.p.rapidapi.com/submissions/" +
+            //   res.token +
+            //   "?base64_encoded=false&fields=*";
+            const url = `https://judge0-ce.p.rapidapi.com/submissions/${res?.token}?base64_encoded=false&fields=*`;
+            const options = {
+              method: "GET",
+              headers: {
+                "X-RapidAPI-Key":
+                  "312a0d93f0msh88b061e76dae04bp1b370ajsn8921fe8ecea6",
+                "X-RapidAPI-Host": "judge0-ce.p.rapidapi.com",
+              },
+            };
+
+            try {
+              const response = await fetch(url, options);
+              const result = await response.json();
+              console.log(result);
+              setOutput(result);
+            } catch (error) {
+              console.error(error);
+            }
+          }
         });
     },
   });
@@ -98,6 +122,13 @@ function App() {
           id="source_code"
           cols="30"
           rows="10"
+        ></textarea>
+        <textarea
+          name="output_of_code"
+          id="output_of_code"
+          cols="30"
+          rows="10"
+          disabled
         ></textarea>
         <button onClick={formik.handleSubmit}>Submit</button>
       </form>
